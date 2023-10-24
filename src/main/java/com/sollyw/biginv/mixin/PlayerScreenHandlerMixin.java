9f2877444e3eb29cd9022f93b5e26012650479b1
Andrew Grant <andrew.grant@mail.rit.edu>
2023-10-23 21:54:19 -0400

    implement 3x3 inventory crafting grid and overhaul GUI

--- src/main/java/com/sollyw/biginv/mixin/PlayerScreenHandlerMixin.java
+++ src/main/java/com/sollyw/biginv/mixin/PlayerScreenHandlerMixin.java
@@ -1,20 +1,83 @@
 package com.sollyw.biginv.mixin;
 
 import com.sollyw.biginv.BigInvModInfo;
+import com.sollyw.biginv.LockableCraftingSlot;
 import com.sollyw.biginv.ScreenHandlerOverrides;
 import com.sollyw.biginv.ScreenHandlerExt;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.screen.AbstractRecipeScreenHandler;
 import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerScreenHandler.class)
-public abstract class PlayerScreenHandlerMixin implements ScreenHandlerExt {
+public abstract class PlayerScreenHandlerMixin extends AbstractRecipeScreenHandler<RecipeInputInventory> implements ScreenHandlerExt {
+    @Shadow @Final private RecipeInputInventory craftingInput;
     @Unique
     private static final Identifier PLAYER = new Identifier("minecraft", "player");
+
+    public PlayerScreenHandlerMixin(ScreenHandlerType<?> screenHandlerType, int i) {
+        super(screenHandlerType, i);
+    }
+
+
+    @ModifyConstant(method = "<init>",
+            constant = {
+                    @Constant(intValue = 2,
+                            ordinal = 0),
+                    @Constant(intValue = 2,
+                            ordinal = 1)})
+    private int invcrafting$craftingInvSize(int value) {
+        return 3;
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerInventory;ZLnet/minecraft/entity/player/PlayerEntity;)V",
+            constant = @Constant(intValue = 2, ordinal = 2))
+    public int invcrafting$makeForLoopNotRun(int constant) {
+        // Vanilla Minecraft adds the 2x2 crafting slots inm a loop. Instead of modifying it, I will stop the loop from running
+        // and add slots individually (since I need to add slots both before and after the player inventory
+        return -1;
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/entity/player/PlayerInventory;ZLnet/minecraft/entity/player/PlayerEntity;)V",
+            at = @At(target = "Lnet/minecraft/screen/PlayerScreenHandler;addSlot(Lnet/minecraft/screen/slot/Slot;)Lnet/minecraft/screen/slot/Slot;",
+                    value = "INVOKE", ordinal = 0, shift = At.Shift.AFTER))
+    private void invcrafting$swapCraftingInput2(PlayerInventory inventory, boolean onServer, PlayerEntity owner, CallbackInfo ci) {
+        this.addSlot(new LockableCraftingSlot(owner, this.craftingInput, 0, 80, 18));
+        this.addSlot(new Slot(this.craftingInput, 1, 98, 18));
+        this.addSlot(new Slot(this.craftingInput, 2, 116, 18));
+        this.addSlot(new LockableCraftingSlot(owner, this.craftingInput, 3, 80, 36));
+    }
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    public void invcrafting$PlayerScreenHandler(PlayerInventory inventory, boolean onServer, PlayerEntity owner, CallbackInfo ci) {
+        this.addSlot(new Slot(this.craftingInput, 4, 98, 36));
+        this.addSlot(new Slot(this.craftingInput, 5, 116, 36));
+        this.addSlot(new LockableCraftingSlot(owner, this.craftingInput, 6, 80, 54));
+        this.addSlot(new LockableCraftingSlot(owner, this.craftingInput, 7, 98, 54));
+        this.addSlot(new LockableCraftingSlot(owner, this.craftingInput, 8, 116, 54));
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerInventory;ZLnet/minecraft/entity/player/PlayerEntity;)V",
+            constant = @Constant(intValue = 154, ordinal = 0))
+    public int tifyi$moveCraftingResultSlotX(int constant) {
+        return 152;
+    }
+
+    @ModifyConstant(method = "<init>(Lnet/minecraft/entity/player/PlayerInventory;ZLnet/minecraft/entity/player/PlayerEntity;)V",
+            constant = @Constant(intValue = 28, ordinal = 0))
+    public int tifyi$moveCraftingResultSlotY(int constant) {
+        return 36;
+    }
 
     /**
      * @author SollyW
@@ -100,6 +163,16 @@ public abstract class PlayerScreenHandlerMixin implements ScreenHandlerExt {
                             ordinal = 0))
     private int offhandEnd(int value) {
         return 70;
+    }
+
+    /**
+     * @author Andrew6rant (Andrew Grant)
+     * @reason I made the inventory crafting grid 3x3 (+ output slot)
+     * It returns 10 always even if the 2x2 grid is valid because the disabled slots are still there
+     */
+    @Overwrite
+    public int getCraftingSlotCount() {
+        return 10;
     }
 
     @Override
--- src/main/java/com/sollyw/biginv/mixin/client/InventoryScreenMixin.java
+++ src/main/java/com/sollyw/biginv/mixin/client/InventoryScreenMixin.java
@@ -1,34 +1,47 @@
 package com.sollyw.biginv.mixin.client;
 
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Items;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InventoryScreen.class)
-public abstract class InventoryScreenMixin {
-    @Shadow
-    private boolean narrow;
+public abstract class InventoryScreenMixin extends AbstractInventoryScreen<PlayerScreenHandler> implements RecipeBookProvider {
 
-    @Inject(method = "init",
-            at = @At(value = "FIELD",
-                    target = "Lnet/minecraft/client/gui/screen/ingame/InventoryScreen;narrow:Z",
-                    opcode = Opcodes.PUTFIELD,
-                    shift = At.Shift.AFTER))
-    private void init(CallbackInfo ci) {
-        //this.narrow = true;
+    @Shadow @Final private RecipeBookWidget recipeBook;
+
+    @Shadow private boolean mouseDown;
+
+    public InventoryScreenMixin(PlayerScreenHandler screenHandler, PlayerInventory playerInventory, Text text) {
+        super(screenHandler, playerInventory, text);
     }
 
-    /*@ModifyConstant(method = "drawBackground(Lnet/minecraft/client/gui/DrawContext;FII)V",
-            constant = @Constant(intValue = 30, ordinal = 0))
-    public int tifyi$changePlayerRenderSize(int constant) {
-        return playerRenderSize;
-    }*/
+    @Inject(method = "drawBackground", at = @At(value = "INVOKE",
+           target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", shift = At.Shift.AFTER))
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY, CallbackInfo ci) {
+        int i = this.x;
+        int j = this.y;
+        if (this.client.player.getInventory().contains(Items.CRAFTING_TABLE.getDefaultStack())) {
+            context.drawTexture(new Identifier("biginv:textures/gui/inventory.png"), i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        } else {
+            context.drawTexture(new Identifier("biginv:textures/gui/inventory_grid_disabled.png"), i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        }
+    }
 
     @ModifyConstant(method = "drawBackground(Lnet/minecraft/client/gui/DrawContext;FII)V",
             constant = {@Constant(intValue = 51, ordinal = 0),@Constant(intValue = 51, ordinal = 1)})
@@ -40,6 +53,26 @@ public abstract class InventoryScreenMixin {
             constant = {@Constant(intValue = 75, ordinal = 0),@Constant(intValue = 75, ordinal = 1)})
     public int tifyi$movePlayerRenderY(int constant) {
         return 72;
+    }
+
+    @ModifyArg(method = "init()V", index = 8, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TexturedButtonWidget;<init>(IIIIIIILnet/minecraft/util/Identifier;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)V"))
+    private ButtonWidget.PressAction tifyi$modifyRecipeBookXY(ButtonWidget.PressAction action) {
+        return (buttonWidget) -> {
+            this.recipeBook.toggleOpen();
+            this.x = this.recipeBook.findLeftEdge(this.width, this.backgroundWidth);
+            buttonWidget.setPosition(this.x + 142, this.height / 2 - 24);
+            this.mouseDown = true;
+        };
+    }
+
+    @ModifyConstant(method = "init()V", constant = @Constant(intValue = 104))
+    private int tifyi$moveRecipeBookX(int constant) {
+        return 142;
+    }
+
+    @ModifyConstant(method = "init()V", constant = @Constant(intValue = 22, ordinal = 0))
+    private int tifyi$moveRecipeBookY(int constant) {
+        return 24;
     }
 
     /*
